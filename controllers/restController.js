488093eccae2d8711d83eb3195a3bdeda5c4570c
Lettuce Lu <lettuce.lu@lettucelu.com>
2021-11-18 22:53:29 +0800

    feat: add restaurants nav-pill feature to select category

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,8 +4,17 @@ const Category = db.Category
 
 const restController = {
   getRestaurants: (req, res) => {
+
+    const whereQuery = {}
+    let categoryId = ''
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)
+      whereQuery.categoryId = categoryId
+    }
+
     Restaurant.findAll({
-      include: [Category]
+      include: [Category],
+      where: whereQuery
     }).then(restaurants => {
       // console.log(restaurants)
       const data = restaurants.map(r => ({
@@ -13,7 +22,18 @@ const restController = {
         description: r.dataValues.description.substring(0, 50),
         categoryName: r.Category.name
       }))
-      return res.render('restaurants', { restaurants: data })
+
+      Category.findAll({
+        raw: true,
+        nest: true
+      }).then(categories => {
+        return res.render('restaurants', {
+          restaurants: data,
+          categories: categories,
+          categoryId: categoryId
+        })
+      })
+
     })
   },
 
