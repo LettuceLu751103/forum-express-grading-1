2d0442124acaf771a27862e0b29f5714a41a24e2
Lettuce Lu <lettuce.lu@lettucelu.com>
2021-11-24 01:51:55 +0800

    GET /api/admin/categories - 後台瀏覽全部類別

---
+++ controllers/api/categoryController.js
@@ -0,0 +1,18 @@
+const db = require('../../models')
+const Category = db.Category
+const categoryService = require('../../services/categoryService')
+
+
+const categoryController = {
+    getCategories: (req, res) => {
+        categoryService.getCategories(req, res, data => {
+            if (req.params.id) {
+                return res.json(data)
+            } else {
+                return res.json(data)
+            }
+        })
+    },
+}
+
+module.exports = categoryController
--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,21 +1,15 @@
 const db = require('../models')
 const Category = db.Category
+const categoryService = require('../services/categoryService')
 
 let categoryController = {
     getCategories: (req, res) => {
-        return Category.findAll({
-            raw: true,
-            nest: true
-        }).then(categories => {
+        categoryService.getCategories(req, res, data => {
             if (req.params.id) {
-                Category.findByPk(req.params.id)
-                    .then(category => {
-                        return res.render('admin/categories', { categories: categories, category: category.toJSON() })
-                    })
+                return res.render('admin/categories', data)
             } else {
-                return res.render('admin/categories', { categories: categories })
+                return res.render('admin/categories', data)
             }
-
         })
     },
 
